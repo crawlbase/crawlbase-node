54e07dba1584b179d7d957c23fdd9e3c454a5f85
crawlbase <info@crawlbase.com>
2024-06-20 10:26:15 +0800

    fix: bring back pcStatus

--- src/crawling-api.js
+++ src/crawling-api.js
@@ -66,10 +66,10 @@ class CrawlingAPI extends BaseAPI {
     return super.processResponse(response).then((response) => {
       if (undefined !== response.headers && undefined !== response.headers.original_status) {
         response.originalStatus = response.headers.original_status * 1;
-        response.cbStatus = (response.headers.cb_status || response.headers.pc_status) * 1;
+        response.cbStatus = response.pcStatus = (response.headers.cb_status || response.headers.pc_status) * 1;
       } else if (undefined !== response.json) {
         response.originalStatus = response.json.original_status * 1;
-        response.cbStatus = (response.json.cb_status || response.json.pc_status) * 1;
+        response.cbStatus = response.pcStatus = (response.json.cb_status || response.json.pc_status) * 1;
       }
       return response;
     });
